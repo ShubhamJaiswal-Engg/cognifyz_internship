35b3b14e67a04cf7dce3276bad974c7f28a9a04b
Shubham Jaiswal <shubhamjaiswal@Shubhams-MacBook-Air.local>
2024-11-21 23:08:47 +0530

    initial commit

---
+++ task1/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+// Serve your index.html file
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/submit-form', (req, res) => {
+    const { name, email } = req.body;
+    res.render('submission', { name });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
